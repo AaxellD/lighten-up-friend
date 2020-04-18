266d399279f4f724692f26f9ccd3fbb6fceb1167
Jesus Vasquez <jesusvasquez@Jesuss-iMac.local>
2020-04-18 12:15:34 -0500

    took out extra aside stuff

--- src/components/aside.js
+++ src/components/aside.js
@@ -17,22 +17,7 @@ class Aside extends React.Component {
           <li onClick={() => { 
             this.props.handleView('addJoke') 
           }}>Add joke</li>
-
-          <li onClick={() => {
-            this.props.handleView('jod')
-          }}>Joke Of The Day</li>
-
-          <li onClick={() => {
-            this.props.handleView('animal')
-          }}>Animal Joke of the Day </li>
-
-          <li onClick={() => {
-            this.props.handleView('blonde')
-          }}>Blonde Joke of the Day </li>
-
-          <li onClick={() => {
-            this.props.handleView('knock')
-          }}>Knock Knock Joke of the Day </li>
+      
         </ul>
 
       </aside>
